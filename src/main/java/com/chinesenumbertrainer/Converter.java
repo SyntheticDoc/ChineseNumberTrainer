ee625d10693bd04b95e3a7b0ba7f8730de8d5a14
SyntheticDoc <e0604573@student.tuwien.ac.at>
2025-11-21 10:33:50 +0100

    Added comment to count significances in Converter.java

--- src/main/java/com/chinesenumbertrainer/Converter.java
+++ src/main/java/com/chinesenumbertrainer/Converter.java
@@ -37,6 +37,8 @@ public class Converter {
 	return result;
     }
     
+    // 两  兆  两  千  两  百  二  十  二  亿  两  千  两  百  二  十  二  万  两  千  两  百  二  十  二
+    // 25  24 23  22  21 20  19  18  17 16  15  14 13  12  11 10  9   8   7  6   5  4   3   2   1
     public String convertFromGermanToChinese(String n) {
 	String result = "";
 	int curSignificance;
