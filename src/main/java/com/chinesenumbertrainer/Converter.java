3a58eaafbb8bc8cd713b4e82a1ae5f23b52ef2ed
SyntheticDoc <e0604573@student.tuwien.ac.at>
2025-12-05 10:58:11 +0100

    Even more converter error fixing...

--- src/main/java/com/chinesenumbertrainer/Converter.java
+++ src/main/java/com/chinesenumbertrainer/Converter.java
@@ -4,6 +4,8 @@
  */
 package com.chinesenumbertrainer;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author TheDoctor
@@ -37,6 +39,36 @@ public class Converter {
 	return result;
     }
     
+    public String convertFromGermanToChinese_test(String n) {
+	String result = "";
+	
+	ArrayList<String> quadruples = new ArrayList<>();
+	int counter = 0;
+	String temp = "";
+	
+	for(int i = n.length() - 1; i >= 0; i--) {
+	    counter ++;
+	    
+	    if(counter == 4) {
+		quadruples.add(temp);
+		temp = new String();
+		counter = 0;
+	    } else {
+		temp = n.charAt(i) + temp;
+	    }
+	}
+	
+	return result;
+    }
+    
+    public String convertQuadrupleFromGermanToChinese(String n) {
+	String result = "";
+	
+	
+	
+	return result;
+    }
+    
     // 两  兆  两  千  两  百  二  十  二  亿  两  千  两  百  二  十  二  万  两  千  两  百  二  十  二
     // 25  24 23  22  21 20  19  18  17 16  15  14 13  12  11 10  9   8   7  6   5  4   3   2   1
     public String convertFromGermanToChinese(String n) {
@@ -46,6 +78,10 @@ public class Converter {
 	int secondary_sigIndex = 0;
 	boolean isLeadingZero = false;
 	boolean inCurrentZeroField = false;
+	
+	if(n.equals("2222222")) {
+	    System.out.println("Beep!");
+	}
 	
 	for(int i = (n.length() - 1); i >= 0; i--) {
 	    int curNum = n.charAt(i) - 48;
@@ -153,15 +189,19 @@ public class Converter {
 			result = getSingleNumeral(curNum, isOneOrTenSignificance) + result;
 		    } else if((curSignificance - 2) % 4 == 0) {
 			result = getSingleNumeral(curNum, true) + measureUnits[0] + result;
+		    } else if((curSignificance - 3) % 4 == 0) {
+			result = getSingleNumeral(curNum) + measureUnits[1] + result;
+		    } else if((curSignificance - 4) % 4 == 0) {
+			result = getSingleNumeral(curNum) + measureUnits[2] + result;
 		    } else {
-			result = getSingleNumeral(curNum) + measureUnits[sigIndex] + result;
 			sigIndex++;
+			result = getSingleNumeral(curNum) + measureUnits[sigIndex] + result;
 		    }
 		}
 	    }
 	    
-	    console.cprintln("n.length = " + n.length() + ", i = " + i + ", curSignificance = " + curSignificance + ", curNum = " + curNum);
-	    console.cprintln("  curResult: " + result);
+	    // console.cprintln("n.length = " + n.length() + ", i = " + i + ", curSignificance = " + curSignificance + ", curNum = " + curNum);
+	    // console.cprintln("  curResult: " + result);
 	}
 	
 	return result;
